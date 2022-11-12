5480567dbf011b5c56f945d57db48cdeb984655c
Guzmance <00031119@uca.edu.sv>
2022-11-12 00:29:18 -0600

    Add: MongoDB Connection

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'Carlos',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jimena',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: 'Free Shirt',
