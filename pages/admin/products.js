9b460eb5be928b22023f82bb5d6a6098d4608a93
Guzmance <00031119@uca.edu.sv>
2022-11-27 00:27:26 -0600

    Add: Fix Upload Product Image

--- pages/admin/products.js
+++ pages/admin/products.js
@@ -87,9 +87,18 @@ export default function AdminProdcutsScreen() {
                       <td className=" p-5 ">{product.countInStock}</td>
                       <td className=" p-5 ">{product.rating}</td>
                       <td className=" p-5 ">
-                        <Link href={`/admin/product/${product._id}`}>Edit</Link>
+                        <Link
+                          legacyBehavior
+                          href={`/admin/product/${product._id}`}
+                        >
+                          <a type="button" className="default-button">
+                            Edit
+                          </a>
+                        </Link>
                         &nbsp;
-                        <button>Delete</button>
+                        <button className="default-button" type="button">
+                          Delete
+                        </button>
                       </td>
                     </tr>
                   ))}
